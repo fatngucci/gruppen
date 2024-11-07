ca7c6d56e82aaceee71ffb80292b59e6a5ff3f8a
Taro Anklam <s86175@bht-berlin.de>
2024-11-07 16:41:29 +0100

    feat: adding sidebar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,56 @@
-import { Share, Menu } from "lucide-react";
+"use client";
+
+import { Box, Drawer, List, ListItem, ListItemButton } from "@mui/material";
+import { Share, Menu, X } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 export default function NavBar() {
+    const [open, setOpen] = React.useState(false);
+
+    const toggleDrawer = (newOpen: boolean) => () => {
+      setOpen(newOpen);
+    }
+
+    const DrawerList = (
+      <Box className="w-[250px]" role="presentation">
+        <List className="justify-center">
+          <ListItem className="self-end" disablePadding>
+            <ListItemButton onClick={toggleDrawer(false)}>
+              <X />
+            </ListItemButton>
+          </ListItem >
+          <ListItem key="home" disablePadding>
+            <ListItemButton>
+              <Link href="/" className=""> Home </Link>
+            </ListItemButton>
+          </ListItem>
+          <ListItem key="regeln" disablePadding>
+            <ListItemButton>
+              <Link href="/regeln"> Regeln </Link>
+            </ListItemButton>
+          </ListItem>
+          <ListItem key="kontakt" disablePadding>
+            <ListItemButton>
+              <Link href="/kontakt"> Kontakt </Link>
+            </ListItemButton>
+          </ListItem>
+          <ListItem key="info" disablePadding>
+            <ListItemButton>
+              <Link href="/info"> Info für Fachschaften </Link>
+            </ListItemButton>
+          </ListItem>
+          <ListItem key="impressum" disablePadding>
+            <ListItemButton>
+              <Link href="/impressum"> Impressum </Link>
+            </ListItemButton>
+          </ListItem>
+       </List>
+      </Box>
+    )
+
+
     return(
     <div className="flex flex-row bg-neutral-900">
         <div className="basis-3/4 p-1 self-center"> 
@@ -13,7 +62,10 @@ export default function NavBar() {
               <Share className="m-1 p-0.5" size={32} color="black"/>
             </div>
             <div className="border rounded">
-              <Menu className="m-1 p-0.5" size={32} />
+                <Menu className="m-1 p-0.5" size={32} onClick={toggleDrawer(true)} />
+                <Drawer open={open} onClose={toggleDrawer(false)} anchor="right">
+                  {DrawerList}
+                </Drawer>
             </div>
           </div>
         </div>
