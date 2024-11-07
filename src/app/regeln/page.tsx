3fda2c790fc4aa00ae7c20829f25128c80755aa1
Taro Anklam <s86175@bht-berlin.de>
2024-11-07 15:22:39 +0100

    feat: gruppenregel page

---
+++ src/app/regeln/page.tsx
@@ -0,0 +1,108 @@
+
+export default function Gruppenregeln() {
+    return(
+        <div className="container mx-auto p-2 bg-neutral-900">
+            <div className="p-2 space-y-10">
+                <p className="text-2xl text-center font-bold my-2"> Gruppenregeln </p>
+                <p className="mb-10"> 
+                    Unsere oberste Priorität ist ein respektvoller und konstruktiver Austausch in den Gruppen. 
+                    Themen, die gegen die Regeln verstoßen, werden gelöscht und die Verantwortlichen können dauerhaft entfernt werden. 
+                    Bei Fragen zu Ausnahmen könnt ihr die Admins kontaktieren.
+                </p>
+                <div>
+                    <p className="text-lg font-bold my-2">
+                        1. Sprache und Zugehörigkeit
+                    </p>
+                    <p>
+                        Nachrichten sollten auf Deutsch oder Englisch verfasst sein und einen Bezug zum Uni-Leben haben. 
+                        Nur eingeschriebene Studierende der jeweiligen Uni dürfen teilnehmen; 
+                        wer an anderen Unis eingeschrieben ist oder in Gruppen mehrerer Unis aktiv ist, wird entfernt.
+                    </p>
+                </div>
+                <div>
+                    <p className="text-lg font-bold my-2">
+                        2. Keine politischen oder religiösen Inhalte
+                    </p>
+                    <p>
+                        Politische und religiöse Themen und Diskussionen sind in den Gruppen untersagt, unabhängig von der vertretenen Position. 
+                        Dies schließt auch die Nutzung von Länderflaggen und Beiträge von politischen Parteien ein. 
+                        Aufrufe zur Teilnahme an politischen Aktionen sind verboten, es sei denn, sie betreffen direkt das Uni- und Studentenleben und sind nicht politisch motiviert.
+                    </p>
+                </div>
+                <div>
+                    <p className="text-lg font-bold my-2">
+                        3. Kein diskriminierendes Verhalten
+                    </p>
+                    <p>
+                        Rassistische, homophobe, transphobe, sexistische, antisemitische oder ähnliche herabwürdigende Aussagen sind verboten. 
+                        Beleidigende Begriffe oder diskriminierende Äußerungen führen zu einem Ausschluss aus der Gruppe.
+                    </p>
+                </div>
+                <div>
+                    <p className="text-lg font-bold my-2">
+                        4. Kein Spam oder Privatgespräche  
+                    </p>
+                    <p>
+                        Spam-Nachrichten und Privatgespräche ohne Bezug zum Studium sind unerwünscht. Wer Sticker oder Bilder ohne inhaltlichen Mehrwert im Übermaß postet, riskiert einen dauerhaften Bann.
+                    </p>
+                </div>
+                <div>
+                    <p className="text-lg font-bold my-2">
+                        5. Werbung ist verboten  
+                    </p>
+                    <p>
+                        Eigenwerbung für Produkte, Dienstleistungen oder Affiliate-Links ist untersagt. Ausnahmen sind Anzeigen für WGs oder Wohnungen sowie private Verkäufe, die keine Eigenwerbung darstellen.
+                    </p>
+                </div>
+                <div>
+                    <p className="text-lg font-bold my-2">
+                        6. Keine persönlichen Angriffe  
+                    </p>
+                    <p>
+                        Respektlose oder beleidigende Äußerungen gegenüber anderen Mitgliedern sind verboten. Diskussionen sollen sachlich und konstruktiv bleiben.
+                    </p>
+                </div>
+                <div>
+                    <p className="text-lg font-bold my-2">
+                        7. Kein Trolling oder absichtliches Stören  
+                    </p>
+                    <p>
+                        Das absichtliche Entgleisenlassen von Themen durch provokative oder ablenkende Kommentare ist untersagt. Auch das bewusste Auslösen von Streitgesprächen und kodifizierte Sprache (Dog Whistling) sind verboten.
+                    </p>
+                </div>
+                <div>
+                    <p className="text-lg font-bold my-2">
+                        8. Keine Gewaltverherrlichung oder Schadenfreude  
+                    </p>
+                    <p>
+                        Gewaltverherrlichung und hämische Kommentare über Katastrophen, Krankheiten oder Gewaltopfer sind nicht gestattet.
+                    </p>
+                </div>
+                <div>
+                    <p className="text-lg font-bold my-2">
+                        9. Kein kontinuierliches Agenda-Pushing  
+                    </p>
+                    <p>
+                        Das ständige Hervorheben eines Themas oder das Stören anderer Diskussionen ist untersagt.
+                    </p>
+                </div>
+                <div>
+                    <p className="text-lg font-bold my-2">
+                        10. Keine Belästigung                  
+                    </p> 
+                    <p>
+                        Belästigung anderer Gruppenmitglieder innerhalb der Gruppe oder per Direktnachricht ist nicht erlaubt.
+                    </p>
+                </div>
+                <div>
+                    <p className="text-lg font-bold my-2">
+                        11. Keine Spendenaufrufe  
+                    </p>
+                    <p>
+                        Spendenaufrufe sind verboten. Ausnahmen sind nur bei nachgewiesener Gemeinnützigkeit und Uni-Bezug nach Absprache mit den Admins möglich.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
