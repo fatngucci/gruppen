da3fe043df22f72a3217ae64ca2f8ceadacdb50c
Taro Anklam <s86175@bht-berlin.de>
2024-11-07 17:51:43 +0100

    feat: list of groups

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,6 +17,62 @@ export default function Home() {
             Diese Website bietet euch eine Übersicht aller WhatsApp-Gruppen für Erstsemester im Wintersemester 2024/25. 
             Hier könnt ihr schon vor dem Semesterstart Kommilitonen kennenlernen, WGs finden und alle Fragen rund ums Studium klären.
           </p>
+          <div className="space-y-6">
+            <div className="text-center">
+              <p className="text-lg font-bold">TU Berlin</p>
+              <p className="text-sm pb-1 pt-2">
+                https://chat.whatsapp.com/L8qOu9UUqar7CwDqUfoovM
+              </p>
+              <p className="text-sm py-1">
+                https://chat.whatsapp.com/BXtZwZOi5zpIRMzfdZUwwt
+              </p>
+            </div>
+            <div className="text-center">
+              <p className="text-lg font-bold"> HU Berlin </p>
+              <p className="text-sm pb-1 pt-2">
+                https://chat.whatsapp.com/D8ftwWReuw4BkURYXBnyM4
+              </p>
+              <p className="text-sm py-2">
+                https://chat.whatsapp.com/BctwAcoezgG7gGY9sh7cSF
+              </p>
+            </div>
+            <div className="text-center">
+              <p className="text-lg font-bold"> FU Berlin </p>
+              <p className="text-sm py-2">
+                https://chat.whatsapp.com/DhCLbTBXHc7JbJrRgj2a2o
+              </p>
+            </div>
+            <div className="text-center">
+              <p className="text-lg font-bold"> HTW Berlin </p>
+              <p className="text-sm py-2">
+                https://chat.whatsapp.com/L6O4mJ2UNZOAXleSFFGxjg
+              </p>
+            </div>
+            <div className="text-center">
+              <p className="text-lg font-bold"> Uni Potsdam </p>
+              <p className="text-sm py-2">
+                https://chat.whatsapp.com/HUFAxpncF7xADVCXLnj90O
+              </p>
+            </div>
+            <div className="text-center">
+              <p className="text-lg font-bold"> Ersti Gruppen Berlin </p>
+              <p className="text-sm py-2"> 
+                https://chat.whatsapp.com/J8iVHk5exVF7dFOhKVJ1jV
+              </p>
+            </div>
+            <div className="text-center">
+              <p className="text-lg font-bold"> Studenten Jobs </p>
+              <p className="text-sm py-2">
+                https://chat.whatsapp.com/GG8pHL8dONQCVK994kLIaz
+              </p>
+            </div>
+            <div className="text-center">
+              <p className="text-lg font-bold"> Wohungsbörse Berlin </p>
+              <p className="text-sm py-2">
+                https://chat.whatsapp.com/JyjhAfsAHdxAWGt9wtmzeB
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
