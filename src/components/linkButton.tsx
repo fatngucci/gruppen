5fc517f2179230070cdfe47cd6f616c6914130a7
Taro Anklam <s86175@bht-berlin.de>
2024-11-10 13:12:57 +0100

    feat: link button component

---
+++ src/components/linkButton.tsx
@@ -0,0 +1,19 @@
+import { ChevronRight } from "lucide-react"
+
+interface LinkButtonAsset {
+    uni: string,
+    link: string
+}
+
+export default function LinkButton({ uni, link }: LinkButtonAsset) {
+    return(
+        <div className="border rounded my-2">
+            <a href={link} target="_blank" rel="noopener noreferrer">
+                <div className="p-2 flex justify-between">
+                    <p> {uni} </p>
+                    <ChevronRight />
+                </div>
+            </a>
+        </div>
+    )
+}
