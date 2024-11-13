b14c3bf68ebf92f29846ced148d5ae090c67ed99
Taro Anklam <s86175@bht-berlin.de>
2024-11-13 21:23:53 +0100

    style: button color

--- src/components/linkButton.tsx
+++ src/components/linkButton.tsx
@@ -9,9 +9,9 @@ export default function LinkButton({ uni, link }: LinkButtonAsset) {
     return(
         <div className="my-2">
             <a href={link} target="_blank" rel="noopener noreferrer">
-                <div className="p-2 flex justify-between bg-neutral-900 rounded-md">
-                    <p className="font-bold text-[#ffffff]"> {uni} </p>
-                    <ChevronRight color="#ffffff" />
+                <div className="p-2 flex justify-between border border-neutral-900 rounded-md">
+                    <p className="font-bold text-neutral-900"> {uni} </p>
+                    <ChevronRight className="neutral-900" />
                 </div>
             </a>
         </div>
