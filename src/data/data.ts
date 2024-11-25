70a264f3e527eae245ad668d818a330cd5f7be84
Taro Anklam <s86175@bht-berlin.de>
2024-11-26 00:37:53 +0100

    feat: data

---
+++ src/data/data.ts
@@ -0,0 +1,218 @@
+export const AllData: AllDataSets = {
+    TU: {
+        TU1: [
+            "IntroProg 24/25", 
+            "Ana 1&LinA WS24/25",
+            "Informatik TU WS24/25",
+            "Mint Orientierungsstudium",
+            "Mechanik 2",
+            "Einführung in die Informatik WiSe 24/25",
+            "Mechanik 2 WISe 24/25",
+            "GLET 24",
+            "ROrg WiSe24/25",
+            "Mikroökonomik WiSe 24/25",
+            "Technische Informatik WS 24/25",
+            "Statistik 1",
+            "Ana 2 ING",
+            "DTS WISE24/25",
+            "Allgemein",
+            "Einführung in die Medieninformatik",
+            "TU Party 2024",
+            "Wirtschaftsingenieurwesen 25/25",
+            "winfo tu berlin 24",
+            "Brau und Getränketechnologie",
+            "Konstruktion 1 WiSe 24/25"
+        ],
+        TU2: [
+            "TU PARTY GRUPPE 24/25 🪩✨", // disco kugel, sterne
+            "Rechnerorganisation",
+            "Informatik TUB 🐐",
+            "Wirtschaftsprivatrecht 24",
+            "Maschinenbau TU",
+            "Informatik Propädeutikum",
+            "Programmieren I",
+            "Einführung in die Wirtschaftsinformatik",
+            "DTS Wise24/25",
+            "Theoretische Informatik",
+            "Verkehrswesen",
+            "Grundlagen der Elektrotechnik (GLET) WS24/25",
+            "Maschinenbau TU WS 24/25",
+            "Zwifach Arbeitslehre TU",
+            "Elektrotechnik 2024",
+            "Ana2 WISE24/25",
+            "Wirtschaftsingenieurwesen 24",
+            "Energie und Prozesstechnik TUB 🍃",
+            "Physik TU WS 24/25",
+            "Signale und Systeme WiSe 24/25",
+            "Mathematik",
+            "Leichtbau I Gruppe",
+            "Chemie TU WS 24/25",
+            "Architektur TU WS 24/25"
+        ]
+    },
+    HU: {
+        HU1: [
+            "Lehramt HU 🙂↔️",
+            "HU Lehramt Erstis 2024",
+            "HU- BERLIN",
+            "GSL - Mathe",
+            "MA 👩🏻‍🎓👨🏻‍🎓 Lehramt 📚 GS",
+            "GSL - Spowis 🤼‍♂️",
+            "Englisch HU WS 24/25",
+            "Deutsch HU WS 24/25",
+            "Q-Master 2021/2022",
+            "Weiterleitung: M.Ed. Lehramt Master ISS/Gym 👩‍🏫",
+            "Englisch/Deutsch Iss/Gym ERSTIS",
+            "Allgemein",
+            "HU Party 🥳",
+            "Geschichte HU WS 24/25",
+            "Bio18 Grundl. Biochemie",
+            "Mathe HU WS 24/25",
+            "Grundlagen Tier- & Neuro",
+            "HU Geschichte",
+            "Weiterleitung: M.Ed. Biologie 👩‍🏫",
+            "Lehramt Philosophie",
+            "Weiterleitung: M.Ed. Geographie 👩‍🏫",
+            "Wirtschaftpädagogik",
+            "Lehramt Physik",
+            "Queer Erstis",
+            "Chemie HU WS 24/25",
+            "Geschichte Hu Lehramt",
+            "Slawische Sprachen und Literaturen",
+            "Sozialwissenschaften",
+            "Evangelische Theologie Lehramt",
+            "Biophysik HU WS 24/25",
+        ],
+        HU2: [
+            "HU PARTY GRUPPE 24/25 🪩✨",
+            "HumboldtKleinanzeigen",
+            "Lehramt Gym/ISS",
+            "Englisch Master",
+            "Biologie HU WS 24/25",
+            "Geographie HU WS 24/25",
+            "Zertifikatsstudium MA/De/Sa 2024/25",
+            "Allgemein",
+            "Queere Erstis"
+        ]
+    },
+    FU: {
+        FU1: [
+            "Lehramt FU WS 24/25",
+            "FU PARTY GRUPPE 24/25 🪩✨",
+            "Geschichte FU WS 24/25",
+            "Deutsch Philologie",
+            "Sonderpädagogik FU WS 24/25",
+            "Englisch FU WS 24/25",
+            "Biologie FU WS 24/25",
+            "CoMa WiSe24/25",
+            "Philosophie FU WS 24/25",
+            "Politik FU WS 24/25",
+            "Naturwissenschaften FU WS 24/25",
+            "Queere Erstis",
+            "Pferdewissenschaften WS24"
+        ]
+    },
+    UDK: {
+        UDK1: [
+            "Allgemein",
+            "Bildende Kunst",
+            "Bühnenbild",
+            "Design",
+            "Gesang/Musiktheater",
+            "Kirchenmusik",
+            "Komposition",
+            "Kostüm Bild",
+            "Kunst und Medien",
+            "Musical/Show",
+            "Schauspiel Studium"
+        ]
+    },
+    UniPotsdam: {
+        UniPotsdam1: [
+            "UP Ersi Party 🥳",
+            "Allgemein",
+            "Uni of Potsdam winter 2024-25",
+            "Anglistik/Amerikanistik Erstis 24/25 🇬🇧 🇦🇺 🇺🇸",
+            "Kulturwissenschaften-Erstis",
+            "VWL Erstis",
+            "Queere Erstis 🌈",
+            "Gymnasiallehramt Mathematik",
+            "Lehramt",
+            "UP Linguistik Erstis",
+            "Ernährungswissenschaften Uni Potsdam",
+            "Förderpädagogik",
+            "Sporttherapie UP",
+            "Geschichte, Politik und Gesellschaft",
+            "Lehramt Inklusion",
+            "Mathematik UP",
+            "Gymnasiallehramt Sport",
+            "Musik Lehramt 🎼",
+            "Uni Potsdam Chemie Lehramt",
+            "medienwissenschaften",
+            "Informatik Lehramt 🏫",
+            "Erziehungswissenschaften & Soziologie",
+            "Förderpädagogik + Mathe",
+            "BWL Erstis 24",
+            "Master WDT (Wirtschaftinformatik u. Digitale Transformation"
+        ]
+    },
+    HTW: {
+        HTW1: [
+            "Allgemein",
+            "HTW Party",
+            "[AI] Angewandte Informatik",
+            "IKG 2024 WS",
+            "BWL HTW",
+            "E-Sports Uni Club",
+            "Design",
+            "Elektrotechnik (Master)",
+            "Cyber Secruity and Business WS24/25",
+            "Energie und Umwelt"
+        ]
+    },
+    HWR: {
+        HWR1: [
+            "Business Administration",
+            "BWL- alle Vertiefungen",
+            "Allgemein",
+            "Informatik",
+            "Bauingenieurwesen",
+            "International Business Management",
+            "International Management",
+            "Rechtspflege",
+            "Sicherheitsmanagement",
+            "Öffentliche Verwaltung"
+        ]
+    },
+    Ersti: {
+        Ersti1: [
+            "Humboldt-Universität zu Berlin",
+            "PARTY GRUPPE 💃🏿🪩",
+            "Technische Universität Berlin",
+            "Freie Universität Berlin",
+            "Charité - Univeritätsmedizin Berlin",
+            "HWR Berlin",
+            "HTW Berlin",
+            "Universität Potsdam",
+            "Universität der Künste Berlin"
+        ]
+    },
+    Partys: {
+        Partys1: [
+            "ALLE STUDENTEN PARTYS IN BLN 2.0"
+        ]
+    },
+    Jobs: {
+        Jobs1: [
+            "STUDENTEN JOBS - BERLIN"
+        ]
+    },
+    Wohnung: {
+        Wohnung1: [
+            "ICH SUCHE",
+            "ICH BIETE",
+            "WG ZIMMER"
+        ]
+    }
+}
+
